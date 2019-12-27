8a932e59bd845f5e9f232b09065f2714fcf2db5d
vinh <vinh@vanteca.com>
2019-12-27 06:28:11 -0500

    Create product list component

---
+++ src/app/products.ts
@@ -0,0 +1,13 @@
+export const products = [{
+    name: 'Angular Development with Typescript',
+    price: 37.54,
+    description: 'Develop angular apps using typescript'
+},{
+    name: 'Elasticsearch in Action',
+    price: 30.25,
+    description: 'Index and search text using elasticsearch'
+},{
+    name: 'MongoDB in Action',
+    price: 32.10,
+    description: ''
+}];
